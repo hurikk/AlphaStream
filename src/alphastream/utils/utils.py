79aaa53447e2942daaafe8f7241d487ea814158f
hurikk <huanprofs033@gmail.com>
2026-09-06 17:13:27 -0300

    docs: add type hints and docstrings to utils\utils.py

--- src/alphastream/utils/utils.py
+++ src/alphastream/utils/utils.py
@@ -1,15 +1,16 @@
 import requests
 
-def get_tickers() -> list:
-    """
-    
-    Get the tickers for all stocks listed on the B3
-    
-    Returns: A list of all the stock tickers listed on B3
-    
+
+def get_tickers() -> list[str]:
+    """Gets the tickers for all stocks listed on B3.
+
+    Fetches the list of available tickers from the Brapi public API.
+
+    Returns:
+        A list of stock ticker strings listed on B3.
     """
     endpoint = "https://brapi.dev/api/available"
     response = requests.get(endpoint)
     data = response.json()
-    tickers = data['stocks']
+    tickers = data["stocks"]
     return tickers
